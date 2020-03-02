48fca2a12bd25daad75fad1b2b11a71c2e47bff7
HtetAungHlaing-code <smileknight76@gmail.com>
2020-03-02 10:50:02 +0630

    first commit

---
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test_two)
+    }
+}
+
+
+
+
